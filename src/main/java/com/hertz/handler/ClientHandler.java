e8ee6535c8468fd077397f4ff486bc2270f0e270
DashDZ <shayan68dashtizad92@gmail.com>
2025-06-12 18:52:20 +0330

    Going back to previous commit //ShayanDZ

--- src/main/java/com/hertz/handler/ClientHandler.java
+++ src/main/java/com/hertz/handler/ClientHandler.java
@@ -118,27 +118,24 @@ public class ClientHandler extends Thread {
     private JsonObject handleLogIn(JsonObject payload) {
         String username = payload.get("username").getAsString();
         String password = payload.get("password").getAsString();
-
-        User user = UserRepository.getInstance().getAllUser().stream()
-                .filter(u -> u.getUsername().equals(username))
-                .findFirst()
+        User temp = UserRepository.getInstance().getAllUser().stream().findAny()
+                .filter(user -> user.getUsername().equals(username) && verifyPassword(user.getHashedPassword(),password))
                 .orElse(null);
+        boolean test = UserRepository.getInstance().getAllUser().stream()
+                .anyMatch(user -> user.getUsername().equals(username) && !verifyPassword(user.getHashedPassword(),password));
 
         JsonObject response = new JsonObject();
-
-        if (user != null) {
-            if (verifyPassword(user.getHashedPassword(), password)) {
-                response.addProperty("status", Response.logInSuccess.toString());
-                response.addProperty("message", "User logged in successfully");
-            } else {
-                response.addProperty("status", Response.incorrectPassword.toString());
-                response.addProperty("message", "Password is incorrect");
-            }
-        } else {
+        if (temp!=null) {
+            response.addProperty("status", Response.logInSuccess.toString());
+            response.addProperty("message", "User logged in successfully");
+        } else if(test){
+            response.addProperty("status", Response.incorrectPassword.toString());
+            response.addProperty("message", "Password is Incorrect");
+        }
+        else {
             response.addProperty("status", Response.userNotFound.toString());
             response.addProperty("message", "Invalid username");
         }
-
         return response;
     }
 
